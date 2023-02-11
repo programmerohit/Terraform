c62309a2174937b0e50c3c1c7e4e171a6c37ee94
Rohit Kumar <41570354+programmerohit@users.noreply.github.com>
2023-02-12 03:34:54 +0530

    Create Deploy Webserver in GCP.tf

---
+++ Deploy Webserver in GCP.tf
@@ -0,0 +1,42 @@
+provider "google" {
+  credentials = file("account.json")
+  project     = "sap-ariba-cobalt"
+  region      = "us-central1"
+}
+
+resource "google_compute_instance" "webserver" {
+  name         = "webserver"
+  machine_type = "f1-micro"
+  zone         = "us-central1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // This will create an external IP address
+    }
+  }
+
+  metadata = {
+    ssh-keys = "user:ssh-rsa ... "
+  }
+}
+
+resource "google_compute_firewall" "webserver" {
+  name    = "webserver"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["webserver"]
+}
